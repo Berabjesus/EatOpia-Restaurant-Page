fc2b1914ee5221b437fe7d69e884df8d0354c7d8
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-01 21:11:59 +0300

    ADD menu image and text content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './scss/menu.scss';
 import createNavbar from './components/navbar';
 import homePage from './pages/home'
 
+// pleasasasdasdasdasdasdadas
+
+import menuPage from './pages/menu';
+
+
+// asdasdasdasdasdasasd
+
 const body = document.getElementsByTagName('body')[0]
 body.appendChild(createNavbar())
 
-homePage()
+menuPage()
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -10,7 +10,35 @@ const menuPage = () => {
 }
 
 const drawBlackBoard = (container) => {
-  
+  const section = document.createElement('section')
+  section.classList.add('d-flex', 'flex-column','pr-4','menu-section-one','w-100', 'h-100vh', 'fade_in')
+
+  const header = document.createElement('h1')
+  header.classList.add('font-weight-bolder', 'main-font', 'border-bottom', 'border-dark','text-center', 'pb-3')
+  header.innerText = 'Daily Specials'
+
+  const imageContainer = document.createElement('div')
+  imageContainer.classList.add('menu-image-container', 'w-100', 'h-100')
+  const image = document.createElement('img')
+  getFunction.setImageProperties(image, blackBoardPicture, 'w-100', 'h-100', 'menu-image')
+
+  const menuTextContainer = document.createElement('div')
+  menuTextContainer.classList.add('menu-text-container', 'd-flex', 'flex-column')
+  const menuHeader = document.createElement('h1')
+  menuHeader.innerText = "Today's Specials"
+  const menuItems = ['Local Doro Wot', 'Local Key wot', 'Burger', 'Pizza']
+  menuTextContainer.appendChild(menuHeader)
+  for (const index in menuItems) {
+    let newText = document.createElement('h4')
+    newText.innerText = (parseInt(index) + 1)+ '. ' + menuItems[index]
+    menuTextContainer.appendChild(newText)
+  }
+  imageContainer.appendChild(image)
+  imageContainer.appendChild(menuTextContainer)
+
+  section.appendChild(header)
+  section.appendChild(imageContainer)
+  container.appendChild(section)
 }
 
 export default menuPage;
